46e0cb8340018694a3de60b507976306a20a3b97
Esteban Padilla <esteban.padilla@sephora.com>
2023-07-26 19:05:08 -0600

    Fixed Score console error.

--- game/js/controllers/scores/scoresService.js
+++ game/js/controllers/scores/scoresService.js
@@ -1,24 +1,25 @@
-import {Service} from "../service.js";
+import { Service } from "../service.js";
+import { Score } from '../../models/score.js';
 
-export class ScoresService extends Service{
-    constructor(controller){
+export class ScoresService extends Service {
+    constructor(controller) {
         super(controller);
     }
 
-    getScores(){
+    getScores() {
         var scores = [];
         var url = `https://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/scores`;
         var request = new XMLHttpRequest();
         request.open('get', url);
         request.onload = () => {
-            if (request.status === 200){
+            if (request.status === 200) {
                 var data = JSON.parse(request.response);
                 data.forEach(scoreData => {
-                let score = new Score(scoreData.clicks, scoreData.score, scoreData.time, scoreData.username);
-                scores.push(score);
+                    let score = new Score(scoreData.clicks, scoreData.score, scoreData.time, scoreData.username);
+                    scores.push(score);
                 });
-            }else{
-                console.error('Error Requesting scores'); 
+            } else {
+                console.error('Error Requesting scores');
             }
             this.controller.showScores(scores);
         }
